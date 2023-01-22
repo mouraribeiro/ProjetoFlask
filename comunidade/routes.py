0da3aa59013f9d1b95a91517082f68857704ebd6
rayanne <mouraribeiro11@gmail.com>
2023-01-21 21:38:04 -0300

    mudar foto perfil

--- comunidade/routes.py
+++ comunidade/routes.py
@@ -32,7 +32,7 @@ def login():
     form_criar_conta = FormCriarConta()
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha.encode('utf-8'),form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.encode('utf-8'), form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
             flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
             par_next = request.args.get('next')
@@ -45,7 +45,7 @@ def login():
     if form_criar_conta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criar_conta.senha.data).decode('utf-8')
         usuario = Usuario(username=form_criar_conta.username.data, email=form_criar_conta.email.data,
-                              senha=senha_cript)
+                          senha=senha_cript)
         database.session.add(usuario)
         database.session.commit()
         flash(f"Conta criada com sucesso, bem vindo(a) {form_criar_conta.email.data}", 'alert-success')
@@ -62,7 +62,7 @@ def sair():
 @app.route('/perfil')
 @login_required
 def perfil():
-    foto_perfil = url_for('static',filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('perfil.html', foto_perfil=foto_perfil)
 
 
@@ -71,17 +71,17 @@ def perfil():
 def criar_post():
     return render_template('criarpost.html')
 
+
 def salvar_imagem(imagem):
     codigo = secrets.token_hex(8)
     nome, extensao = os.path.splitext(imagem.filename)
     nome_arquivo = nome + codigo + extensao
     caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo)
-    tamanho = (400,400)
+    tamanho = (400, 400)
     imagem_reduzida = Image.open(imagem)
     imagem_reduzida.thumbnail(tamanho)
     imagem_reduzida.save(caminho_completo)
     return nome_arquivo
-
 
 
 @app.route('/perfil/editar', methods=['GET', 'POST'])
